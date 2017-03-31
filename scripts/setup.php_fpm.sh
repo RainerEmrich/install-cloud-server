35b350f53d636154a4c54b99d0162dc5b0c3fceb
RainerEmrich <rainer@emrich-ebersheim.de>
2017-03-31 16:41:09 +0000

    Install php7.1 modules too.
    	modified:   scripts/setup.php_fpm.sh

--- scripts/setup.php_fpm.sh
+++ scripts/setup.php_fpm.sh
@@ -40,6 +40,10 @@ setup_php_fpm () {
 		apt-get update
 		apt-get install php7.0-fpm php7.1-fpm
 
+		PACKAGES=""
+		for PACKAGE in $(dpkg -l | grep php7.0 | awk '{print $2}' | sed 's/7.0/7.1/') ; do PACKAGES="${PACKAGES} ${PACKAGE}"; done
+		apt-get install ${PACKAGES} -y
+
 		patch /etc/php/7.0/fpm/php.ini ${PATCH_DIR}/etc.php.7.0.fpm.php.ini.patch
 		patch /etc/php/7.1/fpm/php.ini ${PATCH_DIR}/etc.php.7.1.fpm.php.ini.patch
 		patch /etc/php/7.0/fpm/pool.d/www.conf ${PATCH_DIR}/etc.php.7.0.fpm.pool.d.www.conf.patch
@@ -48,7 +52,7 @@ setup_php_fpm () {
 		systemctl restart php7.0-fpm
 		systemctl restart php7.1-fpm
 
-		a2dismod php7.0 mpm_prefork
+		a2dismod php7.0 php7.1 mpm_prefork
 		a2enmod proxy_fcgi setenvif mpm_event
 
 		patch /etc/apache2/conf-available/php7.0-fpm.conf ${PATCH_DIR}/etc.apache2.conf-available.php7.0-fpm.conf.patch
